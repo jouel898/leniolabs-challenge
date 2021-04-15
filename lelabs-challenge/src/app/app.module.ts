270d6cf9a77d4c56286c5aa069d3ee5aaf4f8825
joel_leon <joeljesus.leon@thomsonreuters.com>
2021-04-15 00:14:49 -0300

    An error adding material module fixed

--- lelabs-challenge/src/app/app.module.ts
+++ lelabs-challenge/src/app/app.module.ts
@@ -4,14 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CongressListComponent } from './components/congress-list/congress-list.component';
-import { MaterialComponent } from './shared/material.component';
 import { ToolbarComponent } from './shared/toolbar/toolbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CongressListComponent,
-    MaterialComponent,
     ToolbarComponent
   ],
   imports: [
