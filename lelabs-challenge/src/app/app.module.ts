a8eb683f6d5c69fb0a75d73e134977961837e6fb
joel_leon <joeljesus.leon@thomsonreuters.com>
2021-04-15 00:13:37 -0300

    Project structure created
    - Component Congress List created, with will be our main view
    - Shared folder created with material module to group all the Angular Material dependencies
    - Toolbar component created to be along with Congress List

--- lelabs-challenge/src/app/app.module.ts
+++ lelabs-challenge/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CongressListComponent } from './components/congress-list/congress-list.component';
+import { MaterialComponent } from './shared/material.component';
+import { ToolbarComponent } from './shared/toolbar/toolbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CongressListComponent,
+    MaterialComponent,
+    ToolbarComponent
   ],
   imports: [
     BrowserModule,
---
+++ lelabs-challenge/src/app/components/congress-list/congress-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-congress-list',
+  templateUrl: './congress-list.component.html',
+  styleUrls: ['./congress-list.component.scss']
+})
+export class CongressListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
